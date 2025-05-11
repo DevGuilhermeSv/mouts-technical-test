b2927818eb19c98a5597581a73419f94d8df2584
Guilherme Rodrigues <development.guilherme@gmail.com>
2025-05-11 10:52:52 -0300

    Add IRepository and IProductRepository interfaces
    - Implement IRepository interface for CRUD operations in IRepository.cs
    - Update IUserRepository to inherit from IRepository
    - Create IProductRepository interface for Product entity operations

---
+++ template/backend/src/Ambev.DeveloperEvaluation.Domain/Repositories/IRepository.cs
@@ -0,0 +1,62 @@
+using Ambev.DeveloperEvaluation.Domain.Common;
+
+namespace Ambev.DeveloperEvaluation.Domain.Repositories;
+
+public interface IRepository<TEntity> where TEntity : BaseEntity
+{
+    
+      /// <summary>
+    /// Creates a new entity in the repository
+    /// </summary>
+    /// <param name="entity">The entity to create</param>
+    /// <param name="cancellationToken">Cancellation token</param>
+    /// <returns>The created entity</returns>
+    Task<TEntity> CreateAsync(TEntity entity, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Retrieves a entity by their unique identifier
+    /// </summary>
+    /// <param name="id">The unique identifier of the entity</param>
+    /// <param name="cancellationToken">Cancellation token</param>
+    /// <returns>The entity if found, null otherwise</returns>
+    Task<TEntity?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Retrieves a entity by their email address
+    /// </summary>
+    /// <param name="email">The email address to search for</param>
+    /// <param name="cancellationToken">Cancellation token</param>
+    /// <returns>The entity if found, null otherwise</returns>
+    Task<TEntity?> GetByEmailAsync(string email, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Deletes a entity from the repository
+    /// </summary>
+    /// <param name="id">The unique identifier of the entity to delete</param>
+    /// <param name="cancellationToken">Cancellation token</param>
+    /// <returns>True if the entity was deleted, false if not found</returns>
+    Task<bool> DeleteAsync(Guid id, CancellationToken cancellationToken = default);
+    
+    /// <summary>
+    /// Retrieves all entities
+    /// </summary>
+    /// <param name="cancellationToken">Cancellation token</param>
+    /// <returns>An IQueryable of TEntitys</returns>
+    IQueryable<TEntity> GetAll(CancellationToken cancellationToken = default);
+    /// <summary>
+    /// Retrieves all entities
+    /// </summary>
+    /// <param name="orderBy">The property to order the results by (e.g., "entity name")</param>
+    /// <param name="orderDir">The direction of the ordering, either "asc" for ascending or "desc" for descending (default: "desc")</param>
+    /// <param name="cancellationToken">Cancellation token to cancel the operation</param>
+    /// <returns>An IQueryable of TEntities</returns>
+    IQueryable<TEntity> GetAll(string orderBy, string? orderDir = "desc",CancellationToken cancellationToken = default);
+    /// <summary>
+    /// Updates a new entity in the repository
+    /// </summary>
+    /// <param name="entity">The entity to update</param>
+    /// <returns>The created entity</returns>
+    void Update(TEntity entity);
+    Task SaveChangesAsync(CancellationToken cancellationToken);
+    
+}
