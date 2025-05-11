ab9056f3d856f341e44850450bac905d9b498a28
Guilherme Rodrigues <development.guilherme@gmail.com>
2025-05-11 18:52:50 -0300

    Add CartRepository to infrastructure module initializer and create new CartRepository class

---
+++ template/backend/src/Ambev.DeveloperEvaluation.ORM/Repositories/CartRepository.cs
@@ -0,0 +1,6 @@
+using Ambev.DeveloperEvaluation.Domain.Entities;
+using Ambev.DeveloperEvaluation.Domain.Repositories;
+
+namespace Ambev.DeveloperEvaluation.ORM.Repositories;
+
+public class CartRepository(DefaultContext context) : Repository<Cart>(context), ICartRepository;
