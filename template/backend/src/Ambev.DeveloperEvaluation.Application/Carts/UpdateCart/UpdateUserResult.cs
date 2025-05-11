15577c085b47ed77516d58ca5e31ba364136fd25
Guilherme Rodrigues <development.guilherme@gmail.com>
2025-05-11 20:57:54 -0300

    - Add `UpdateCartCommand` for handling cart update requests - Implement `UpdateCartHandler` to process and update cart details - Add `UpdateCartResult` to return updated cart information - Create `UpdateCartProfile` for mapping between cart entities and DTOs

---
+++ template/backend/src/Ambev.DeveloperEvaluation.Application/Carts/UpdateCart/UpdateUserResult.cs
@@ -0,0 +1,22 @@
+﻿using Ambev.DeveloperEvaluation.Application.Address;
+using Ambev.DeveloperEvaluation.Domain.Enums;
+
+namespace Ambev.DeveloperEvaluation.Application.Carts.UpdateCart;
+
+/// <summary>
+/// Represents the response returned after successfully updating a new Cart.
+/// </summary>
+/// <remarks>
+/// This response contains the unique identifier of the newly updated Cart,
+/// which can be used for subsequent operations or reference.
+/// </remarks>
+public class UpdateCartResult : BaseCartDto
+{
+    /// <summary>
+    /// Gets or sets the unique identifier of the newly updated Cart.
+    /// </summary>
+    /// <value>A GUID that uniquely identifies the updated Cart in the system.</value>
+    public Guid Id { get; set; }
+    
+   
+}
