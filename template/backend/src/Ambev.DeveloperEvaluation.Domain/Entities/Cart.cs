f9dc1994a0266451d3524531af14cb7e061a3606
Guilherme Rodrigues <development.guilherme@gmail.com>
2025-05-11 18:11:35 -0300

    Added Cart.cs entity and its migrations

---
+++ template/backend/src/Ambev.DeveloperEvaluation.Domain/Entities/Cart.cs
@@ -0,0 +1,55 @@
+using Ambev.DeveloperEvaluation.Domain.Common;
+
+namespace Ambev.DeveloperEvaluation.Domain.Entities;
+
+/// <summary>
+/// Represents a shopping cart associated with a user and a list of products.
+/// </summary>
+public class Cart: BaseEntity
+{
+    /// <summary>
+    /// The ID of the user who owns the cart.
+    /// </summary>
+    public int UserId { get; private set; }
+
+    /// <summary>
+    /// The date the cart was created or updated.
+    /// </summary>
+    public DateTime Date { get; private set; }
+
+    /// <summary>
+    /// Collection of products in the cart with their respective quantities.
+    /// </summary>
+    public List<CartProduct> Products { get; private set; } = new();
+
+    protected Cart() { } 
+
+    public Cart(int userId, DateTime date, List<CartProduct> products)
+    {
+        UserId = userId;
+        Date = date;
+        Products = products ?? throw new ArgumentNullException(nameof(products));
+    }
+}
+
+/// <summary>
+/// Represents a product and its quantity in a cart.
+/// </summary>
+public class CartProduct :BaseEntity
+{
+    public Guid CartId { get; private set; }
+    public Cart Cart { get; private set; }
+    public Guid ProductId { get; private set; }
+    public Product Product { get; private set; }
+    public int Quantity { get; private set; }
+
+    public CartProduct(Guid productId, int quantity, Guid cartId)
+    {
+        if (quantity <= 0)
+            throw new ArgumentOutOfRangeException(nameof(quantity), "Quantity must be at least 1.");
+
+        ProductId = productId;
+        Quantity = quantity;
+        CartId = cartId;
+    }
+}
