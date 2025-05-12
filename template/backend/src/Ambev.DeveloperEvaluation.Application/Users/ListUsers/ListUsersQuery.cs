69482fc640f914b085760157dca14d9d830c96fd
Guilherme Rodrigues <development.guilherme@gmail.com>
2025-05-12 04:09:29 -0300

    Add search and filter functionality to user listing

--- template/backend/src/Ambev.DeveloperEvaluation.Application/Users/ListUsers/ListUsersQuery.cs
+++ template/backend/src/Ambev.DeveloperEvaluation.Application/Users/ListUsers/ListUsersQuery.cs
@@ -1,5 +1,7 @@
+using System.Linq.Expressions;
 using Ambev.DeveloperEvaluation.Application.Common;
 using Ambev.DeveloperEvaluation.Application.Users.GetUser;
+using Ambev.DeveloperEvaluation.Domain.Entities;
 using MediatR;
 
 namespace Ambev.DeveloperEvaluation.Application.Users.ListUsers;
@@ -9,4 +11,16 @@ public class ListUsersQuery : IRequest<PaginatedList<GetUserResult>>
     public int Page { get; set; } = 1;
     public int Size { get; set; } = 10;
     public string? Order { get; set; }
+    
+    public string? Username { get; set; } = string.Empty;
+
+   
+    public string? Password { get; set; } = string.Empty;
+
+   
+    public string? Phone { get; set; } = string.Empty;
+
+    
+    public string? Email { get; set; } = string.Empty;
+
 }
